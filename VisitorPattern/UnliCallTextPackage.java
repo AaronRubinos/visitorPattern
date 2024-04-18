cc8ffd3c83061b619fe12f14ef1816d6b8bbab97
Aaron Gil Rubinos <142986554+AaronRubinos@users.noreply.github.com>
2024-04-18 03:17:26 +0000

    Add visitor pattern

---
+++ VisitorPattern/UnliCallTextPackage.java
@@ -0,0 +1,12 @@
+package VisitorPattern;
+
+public class UnliCallTextPackage implements UnliCallOffer {
+    @Override
+    public String showUnliCallsTextOffer(TelcoSubscription telco) {
+        if (telco.isUnliCallText()) {
+            return telco.getTelcoName() + " offers unlimited calls and texts to all networks within the country.";
+        } else {
+            return telco.getTelcoName() + " does not offer unlimited calls and texts.";
+        }
+    }
+}
